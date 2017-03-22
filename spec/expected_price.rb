12ff776d148d4e0ec6ba2dd306e52ab8719ec15a
S1rFrancis <francisj2011@gmail.com>
2017-03-22 18:14:56 +0000

    Initial push of project structure

---
+++ spec/expected_price.rb
@@ -0,0 +1,11 @@
+require 'money'
+
+I18n.enforce_available_locales = false
+describe Money do
+  it "should be equal" do
+    expected_total_price = Money.new(66_78, "GBP").format
+    actual_toal_price = Money.new(66_78, "GBP").format
+
+   expect(expected_total_price).to eql(actual_toal_price)
+ end
+end
